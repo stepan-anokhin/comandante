82ef7a35f2df42e535d3443c97b49cb7d1e5578c
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-04 14:39:49 +0700

    Add inner package documentation

---
+++ comandante/inner/__init__.py
@@ -0,0 +1,6 @@
+"""Non public API of the comandante.
+
+This package contains definitions that shouldn't
+be used by outside users and that are considered
+as a subject of change in the future.
+"""
