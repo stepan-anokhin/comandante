b8d347f094398096a6fba4e2078dead3d0da376d
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-04 14:37:48 +0700

    Update model

--- comandante/inner/model.py
+++ comandante/inner/model.py
@@ -10,15 +10,42 @@ This model classes are more or less declarations needed
 for parsers and executors to interpret command line
 arguments and execute the corresponding logic.
 """
+
+import inspect
+import itertools
 import re
 
 
-class Option:
-    """Command option descriptor
+class _Empty:
+    """Marker object for Argument.empty"""
 
-    Each command option must have a valid name, type
-    and could be required or not. If option is not
-    required it may have a default value.
+
+class Option:
+    """Command option descriptor.
+
+    Option must have a valid name and a valid short name.
+    Option with name='long' and short-name='s' will have
+    the following syntax:
+
+        --long [value]
+
+    The same using the short name will be:
+
+        -s [value]
+
+    Option value will be parsed according to the option.type.
+    The type must be a callable accepting a single argument. It
+    will be called with the option value from the command-line.
+    The only exception is when option.type=bool. In this case
+    no value is supposed. Option value is supposed to be default
+    when it is not specified and supposed to be True when option
+    is specified (without any value):
+
+        --boolean_option # NOTE: there is no option value
+
+    All options must have a default value by design. If some option
+    must be specified it should be a command argument instead -
+    not an option.
     """
 
     # Regex-pattern of valid option name.
@@ -28,106 +55,225 @@ class Option:
 
     @staticmethod
     def is_valid_name(name):
-        """Check if the given name is a valid option name."""
+        """Validate otion name.
+
+        :param name: option name to be validated
+        :return: True iff name is a valid option name
+        """
         return isinstance(name, str) and bool(Option._name_pattern.fullmatch(name))
 
-    def __init__(self, name, type=str, required=False, default=None):
+    def __init__(self, name, short, type, default):
         """Initialize an instance.
 
-        Arguments:
-        - name - must be a valid option name.
-        - type - option value parser (unary function)
-        - required - is the option required?
-        - default - default option value
-
+        :param name: option long name
+        :param short: option short name
+        :param type: option type
+        :param default: option default value
         """
         if not Option.is_valid_name(name):
-            raise ValueError("'{name}' is not a valid option name".format(name=name))
+            raise ValueError("Not a valid option name: " + str(name))
+
         self._name = name
+        self._short = short
         self._type = type
-        self._required = bool(required)
         self._default = default
 
     @property
     def name(self):
+        """Get option long name."""
+        return self._name
+
+    @property
+    def short(self):
+        """Get option short name."""
+        return self._short
+
+    @property
+    def type(self):
+        """Get option type."""
+        return self._type
+
+    @property
+    def default(self):
+        """Get option default value."""
+        return self._default
+
+
+class Argument:
+    """Argument descriptor.
+
+    Argument is a command parameter that is essential to what
+    command will actually do.
+
+    Each argument must have a valid name derived from the
+    corresponding function (or method) parameter name.
+
+    Each argument must have a type. Argument.type will is a callable
+    that will be used to parse the corresponding string value from
+    the command-line. The only exception is the argument.type=bool,
+    in which case argument name is interpreted as a True (all other
+    values are interpreted as not relevant to the argument).
+    """
+
+    @staticmethod
+    def from_param(param):
+        """Create a new Argument from the given inspect.Parameter.
+
+        :param param: inspect.Parameter from which to derive argument attributes
+        :return: a new Argument instance initialized from the parameter
+        """
+        return Argument(
+            name=param.name,
+            type=Argument._param_type(param),
+            default=Argument._param_default(param)
+        )
+
+    @staticmethod
+    def _param_default(param):
+        """Derive argument default value from the inspect.Parameter default value."""
+        if param.default is inspect.Parameter.empty:
+            return Argument.empty
+        return param.default
+
+    @staticmethod
+    def _param_type(param):
+        """Derive argument type from the parameter annotation"""
+        if param.annotation is inspect.Parameter.empty:
+            return str
+        return param.annotation
+
+    # Marker value indicating that default value is not specified.
+    empty = _Empty
+
+    def __init__(self, name, type, default):
+        """Initialize instance.
+
+        :param name: argument name
+        :param type: argument type
+        :param default: default value (Argument.empty if not present)
+        """
+        self._name = name
+        self._type = type
+        self._default = default
+
+    @property
+    def name(self):
+        """Get argument name."""
         return self._name
 
     @property
     def type(self):
+        """Get argument type."""
         return self._type
 
     @property
+    def default(self):
+        """Get argument default value."""
+        return self._default
+
+    def is_required(self):
+        """Check if argument is required."""
+        return self.default is Argument.empty
+
+
+class Signature:
+    """Command signature descriptor.
+
+    Signature represents a sequence of required
+    and optional command arguments.
+    """
+
+    @staticmethod
+    def from_function(func, is_method=False):
+        """Derive a new `Signature` from the given callable object.
+
+        :param func: a callable object from which to derive signature
+        :param is_method: indicates whether the first argument is `self`
+        :return: a new `Signature` derived from the `func`
+        """
+        sig = inspect.signature(func)
+        required = []
+        optional = []
+        vararg = None
+        accepts_options = False
+
+        params = sig.parameters.items()
+        if is_method:
+            params = itertools.islice(params, 1, None)
+
+        for name, param in params:
+            if param.kind is inspect.Parameter.POSITIONAL_OR_KEYWORD:
+                argument = Argument.from_param(param)
+                category = Signature._determine_category(argument, required, optional)
+                category.append(argument)
+            elif param.kind is inspect.Parameter.VAR_POSITIONAL:
+                vararg = Argument.from_param(param)
+            elif param.kind is inspect.Parameter.VAR_KEYWORD:
+                accepts_options = True
+        return Signature(
+            required=required,
+            optional=optional,
+            vararg=vararg,
+            accepts_options=accepts_options,
+            is_method=is_method
+        )
+
+    @staticmethod
+    def _determine_category(argument, required, optional):
+        """Determine argument category from its default value."""
+        if argument.default is Argument.empty:
+            return required
+        return optional
+
+    @staticmethod
+    def _params(func, is_method):
+        """Extract parameters from the `func` honoring its nature (method or procedure)."""
+        parameters = inspect.signature(func).parameters.items()
+        if is_method:
+            return itertools.islice(parameters, 1, None)
+        return parameters.items()
+
+    def __init__(self, required, optional, vararg=None, accepts_options=False, is_method=False):
+        """Initialize instance.
+
+        :param required: a sequence of required arguments
+        :param optional: a sequence of optional arguments
+        :param vararg: a variable quantity argument descriptor
+        :param accepts_options: indicates whether the underlying python function accepts options as kwargs
+        :param is_method: indicates whether the underlying python function accepts `self` as a first argument
+        """
+        self._required = tuple(required)
+        self._optional = tuple(optional)
+        self._vararg = vararg
+        self._accepts_options = accepts_options
+        self._is_method = is_method
+
+    @property
     def required(self):
+        """Get required arguments."""
         return self._required
 
     @property
-    def default(self):
-        return self._default
+    def optional(self):
+        """Get optional arguments."""
+        return self._optional
 
+    @property
+    def arguments(self):
+        """Get all arguments iterator (required, then optional)."""
+        return itertools.chain(self.required, self.optional)
 
-class Command:
-    """Command descriptor.
+    @property
+    def vararg(self):
+        """Get var-arg descriptor."""
+        return self._vararg
 
-    The Command class represents command declaration.
-    """
-    # Regex-pattern of valid command name.
-    # Valid command name starts with alphabetic character and
-    # its tail contain any number of alpha-numeric characters.
-    _name_pattern = re.compile(r'\a\w+')
+    @property
+    def accepts_options(self):
+        """Check if the underlying python function accepts options as kwargs."""
+        return self._accepts_options
 
-    # Factory helpers:
-
-    @staticmethod
-    def is_valid_name(name):
-        """Check if the given name is a valid command name."""
-        return isinstance(name, str) and bool(Command._name_pattern.fullmatch(name))
-
-    @staticmethod
-    def extract_name(func):
-        """Try to guess command name."""
-        return func.__name__
-
-    @staticmethod
-    def extract_description(func):
-        """Try to guess command description."""
-        return func.__doc__ or ""
-
-    @staticmethod
-    def extract_usage(func):
-        """Try to figure out command usage from target function."""
-        # TODO: implement
-        return ""
-
-    @staticmethod
-    def extract_brief(func):
-        """Try to figure out command brief description from target function."""
-        # TODO: implement
-        return ""
-
-    @staticmethod
-    def from_callable(func):
-        """Create and initialize a new Command descriptor from the given function."""
-        return Command(
-            name=Command.extract_name(func),
-            usage=Command.extract_usage(func),
-            brief=Command.extract_brief(func),
-            description=Command.extract_description(func),
-        )
-
-    # Instance methods:
-
-    def __init__(self, func, name, usage, brief, description=None):
-        """Initialize an instance."""
-        self._func = func
-        self._name = name
-        self._usage = usage
-        self._brief = brief
-        self._description = description
-        self._options = {}
-
-    def define_option(self, name, type=str, required=False, default=None):
-        """Define a command option"""
-        option = Option(name=name, type=type, required=required, default=default)
-        self._options[option.name] = option
-
-    # TODO: implement command properties
+    @property
+    def is_method(self):
+        """Check if the underlying python function is method accepting `self` as a first argument."""
+        return self._is_method
