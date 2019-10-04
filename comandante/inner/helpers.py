e1f2adec3bf18fddf8fa3e812fec5f5b38e17be7
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-04 20:05:50 +0700

    Fix describe helper

--- comandante/inner/helpers.py
+++ comandante/inner/helpers.py
@@ -17,9 +17,13 @@ def describe(o):
     :param o: object to be described.
     :return: (brief, long) description pair
     """
+
+    def isblank(line):
+        return line.isspace() or len(line) == 0
+
     if inspect.getdoc(o) is None:
         return '', ''
     lines = inspect.getdoc(o).split('\n')
     brief = lines[0]
-    descr = itertools.dropwhile(lambda line: line, lines[1:])
+    descr = itertools.dropwhile(isblank, lines[1:])
     return brief, '\n'.join(descr)
