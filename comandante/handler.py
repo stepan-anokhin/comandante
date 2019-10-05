8c76857620f568e299f008c6256810608f8541e2
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-05 22:21:54 +0700

    Implement handler help

--- comandante/handler.py
+++ comandante/handler.py
@@ -12,6 +12,7 @@ import comandante.decorators as decor
 from comandante.errors import CliSyntaxException
 from comandante.inner.bind import BoundCommand, HandlerProxy
 from comandante.inner.helpers import describe
+from comandante.inner.markup import Ansi, DocstringMarkup
 from comandante.inner.model import Option
 
 
@@ -36,12 +37,19 @@ class Handler:
         """
         self._commands = {}
         self._declared_options = {}
-        self._brief, self._descr = describe(self)
+        self._brief, self._descr = self._describe()
         self._options = {}
         for name in dir(self):
             attr = getattr(self, name)
             if type(attr) is BoundCommand:
                 self._commands[name] = attr
+
+    def _describe(self):
+        """Derive description from the docstring."""
+        if type(self) is Handler:
+            # ignore docstring from the generic handler
+            return '', ''
+        return describe(self)
 
     def invoke(self, *argv):
         """Invoke cli-handler with the given raw command-line arguments."""
@@ -63,11 +71,24 @@ class Handler:
 
     @decor.command()
     def help(self, command=None):
-        """A generic implementation of the `help` command.
+        """Display help information about
 
-        :param command: a command name to print help for.
+        With no [command] given, the synopsis of the {name} and a
+        list of commands are printed on the standard output.
         """
-        self._print("from help. Command = {command}".format(command=repr(command)))  # TODO: implement
+        if command is None:
+            self._print(self.full_doc)
+            return
+
+        if command not in self._commands:
+            self._print("Unknown command: {name}".format(name=command))
+            self._print(self.full_doc)
+            return
+
+        self._print(self._commands[command].full_doc)
+
+    def _help_command(self, command_name):
+        """Print help for a particular command."""
 
     def _print(self, *args):
         """Print output. Is convenient to intercept cli-handler's output."""
@@ -83,7 +104,41 @@ class Handler:
         """Get long handler description."""
         return self._descr
 
+    @property
+    def full_doc(self):
+        """Get full documentation"""
+        sections = list()
+        sections.append(self.brief)
+        sections.append(self._command_summary())
+        sections.append(self._description_section())
+        sections = filter(bool, sections)
+
+        return '\n\n'.join(sections)
+
+    def _command_summary(self):
+        """Get summary for all defined commands."""
+        names = []
+        briefs = []
+        for name in sorted(self._commands.keys()):
+            names.append(name)
+            briefs.append(self._commands[name].brief)
+        name_column_width = max(map(len, names)) + 4
+
+        lines = [Ansi.bold('COMMANDS')]
+        for name, brief in zip(names, briefs):
+            lines.append("{name:<{width}}#    {brief}".format(name=name, brief=brief, width=name_column_width))
+        return '\n'.join(lines)
+
+    def _description_section(self):
+        """Get formatted description section."""
+        if not self.descr:
+            return
+        heading = Ansi.bold("DESCRIPTION")
+        description_body = DocstringMarkup.process(self.descr)
+        return "{heading}\n{body}".format(heading=heading, body=description_body)
+
     def subcommand(self, name, handler):
+        """Declare subcommand."""
         if name in self._commands:
             raise RuntimeError("Duplicate command name: {name}".format(name=name))
         self._commands[name] = handler
