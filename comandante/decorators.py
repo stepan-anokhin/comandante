77e2efa7c0b122ab8050c9a5032037855e2c1f06
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-06 22:57:32 +0700

    Fix option decorator

--- comandante/decorators.py
+++ comandante/decorators.py
@@ -27,19 +27,20 @@ def command(name=None):
     return decorator
 
 
-def option(name, short, type, default):
+def option(name, short, type, default, descr):
     """Declare a new option for the given cli-command.
 
     :param name: option name
     :param short: option short name
     :param type: option type
     :param default: option default value
+    :param descr: option description
     :return: a new decorator that will declare option for marked command
     """
 
     def decorator(cmd):
         """Decorator declaring a new option."""
-        cmd.declare_option(name=name, short=short, type=type, default=default)
+        cmd.declare_option(name=name, short=short, type=type, default=default, descr=descr)
         return cmd
 
     return decorator
