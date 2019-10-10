9bf2613be693b2cee91602bfeb4cf645dadde205
Stepan Anokhin <stepan.anokhin@gmail.com>
2019-10-10 23:03:31 +0700

    Write integration tests

--- tests/argument_tests.py
+++ tests/argument_tests.py
@@ -1,36 +1,90 @@
 import unittest
 
 import comandante as cli
+import comandante.errors as error
+from comandante.inner.test import suppress_output, capture_output
 
 
-class ComandanteBasicIntegrationTests(unittest.TestCase):
+class App(cli.Handler):
+    @cli.command()
+    def test_int(self, int_arg: int):
+        return int_arg
+
+    @cli.command()
+    def test_types(self, str_arg, bool_arg: bool, int_arg: int):
+        return str_arg, bool_arg, int_arg
+
+    @cli.command()
+    def test_default(self, required, optional='default'):
+        return required, optional
+
+    @cli.command()
+    def test_no_arguments(self):
+        return 'success'
+
+    @cli.command()
+    def test_vararg(self, required, optional='default', *vararg: int):
+        return required, optional, vararg
+
+
+class ArgumentTests(unittest.TestCase):
+    """Integration tests for cli-argument passing."""
+
     def test_return_value(self):
-        expected = 42
-
-        class Application(cli.Handler):
-            @cli.command()
-            def test(self):
-                return expected
-
-        actual = Application().invoke('test')
-        self.assertEqual(actual, expected)
-
-    def test_string_arguments(self):
-        arguments = ('hello', 'world')
-
-        class Application(cli.Handler):
-            @cli.command()
-            def test(self, first, second):
-                return first, second
-
-        result = Application().invoke('test', *arguments)
-        self.assertEqual(result, arguments)
+        result = App().invoke(['test_no_arguments'])
+        self.assertEqual(result, 'success')
 
     def test_argument_types(self):
-        class Application(cli.Handler):
-            @cli.command()
-            def test(self, argument: int):
-                return argument
+        result = App().invoke('test_types string bool_arg 42'.split())
+        self.assertEqual(result, ('string', True, 42))
 
-        result = Application().invoke('test', '42')
-        self.assertEqual(result, 42)
+    def test_argument_default_value(self):
+        result = App().invoke('test_default required'.split())
+        self.assertEqual(result, ('required', 'default'))
+
+    def test_argument_override_default(self):
+        result = App().invoke('test_default required override'.split())
+        self.assertEqual(result, ('required', 'override'))
+
+    def test_argument_vararg_zero_values(self):
+        result = App().invoke('test_vararg required override'.split())
+        self.assertEqual(result, ('required', 'override', ()))
+
+    def test_argument_vararg_one_value(self):
+        result = App().invoke('test_vararg required override 1'.split())
+        self.assertEqual(result, ('required', 'override', (1,)))
+
+    def test_argument_vararg_multiple_values(self):
+        result = App().invoke('test_vararg required override 1 2 3 4'.split())
+        self.assertEqual(result, ('required', 'override', (1, 2, 3, 4)))
+
+    def test_argument_vararg_no_optional(self):
+        result = App().invoke('test_vararg required '.split())
+        self.assertEqual(result, ('required', 'default', ()))
+
+    def test_argument_invalid_value(self):
+        with suppress_output():
+            self.assertRaises(error.InvalidValue, App().test_int.invoke, 'test_int invalid'.split())
+
+    def test_argument_missing_value(self):
+        with suppress_output():
+            self.assertRaises(error.ArgumentMissing, App().test_default.invoke, ())
+
+    def test_too_many_arguments(self):
+        with suppress_output():
+            self.assertRaises(error.TooManyArguments, App().test_default.invoke, 'required optional extra'.split())
+
+    def test_argument_error_message(self):
+        try:
+            with capture_output() as (out, err):
+                App().test_int.invoke('invalid'.split())
+        except error.InvalidValue as e:
+            self.assertIn(str(e), out.getvalue())
+            self.assertIn('int_arg', str(e))
+
+    def test_help_on_argument_error(self):
+        try:
+            with capture_output() as (out, err):
+                App().test_int.invoke('invalid'.split())
+        except error.InvalidValue as e:
+            self.assertIn(App().test_int.full_doc(), out.getvalue)
---
+++ tests/option_tests.py
@@ -0,0 +1,115 @@
+import unittest
+
+import comandante as cli
+import comandante.errors as error
+from comandante.inner.test import capture_output, suppress_output
+
+
+class App(cli.Handler):
+    @cli.option('some', 's', int, 0)
+    @cli.option('flag', 'f', bool, False)
+    @cli.command()
+    def test(self, **specified_options):
+        return specified_options
+
+    @cli.option('some', 's', int, 0)
+    @cli.option('flag', 'f', bool, False)
+    @cli.command()
+    def test_merge(self, **specified_options):
+        return self.test_merge.options(specified_options)
+
+
+class OptionTests(unittest.TestCase):
+    """Integration tests for cli-options."""
+
+    def test_empty_options(self):
+        result = App().invoke(['test'])
+        self.assertEqual(result, {})
+
+    def test_option_value(self):
+        result = App().invoke('test --some 42'.split())
+        self.assertEqual(result, {'some': 42})
+
+    def test_boolean_option(self):
+        result = App().invoke('test --flag'.split())
+        self.assertEqual(result, {'flag': True})
+
+    def test_short_option_value(self):
+        result = App().invoke('test -s 42'.split())
+        self.assertEqual(result, {'some': 42})
+
+    def test_short_bool_value(self):
+        result = App().invoke('test -f'.split())
+        self.assertEqual(result, {'flag': True})
+
+    def test_mixed_syntax(self):
+        result = App().invoke('test -f --some 42'.split())
+        self.assertEqual(result, {'flag': True, 'some': 42})
+
+    def test_merge_options_default_only(self):
+        result = App().invoke(['test_merge'])
+        self.assertEqual(result, App.test_merge.default_options())
+
+    def test_merge_options_some_specified(self):
+        result = App().invoke('test_merge --some 42'.split())
+        default = App.test_merge.default_options()
+        self.assertEqual(result, {'some': 42, 'flag': default.flag})
+
+    def test_merge_options_all_specified(self):
+        result = App().invoke('test_merge --some 42 --flag'.split())
+        self.assertEqual(result, {'some': 42, 'flag': True})
+
+    def test_merge_options_attr_access(self):
+        options = App().invoke('test_merge --some 42 --flag'.split())
+        self.assertEqual(options.some, 42)
+        self.assertEqual(options.flag, True)
+
+    def test_merge_options_is_specified(self):
+        options = App().invoke('test_merge --some 42'.split())
+        self.assertTrue(options.is_specified('some'))
+        self.assertFalse(options.is_specified('flag'))
+
+    def test_unknown_are_not_specified(self):
+        options = App.test.options({})
+        self.assertFalse(options.is_specified('unknown'))
+
+    def test_merge_options_default_attr_access(self):
+        options = App().invoke(['test_merge'])
+        default = App.test_merge.default_options()
+        self.assertEqual(options.some, default.some)
+        self.assertEqual(options.flag, default.flag)
+
+    def test_default_options(self):
+        default = App.test_merge.default_options()
+        self.assertEqual(default, {'flag': False, 'some': 0})
+
+    def test_unknown_option(self):
+        with suppress_output():
+            self.assertRaises(error.UnknownOption, App().test_merge.invoke, ['--unknown'])
+
+    def test_invalid_option_value(self):
+        with suppress_output():
+            self.assertRaises(error.InvalidValue, App().test_merge.invoke, '--some invalid-value'.split())
+
+    def test_missing_option_value(self):
+        with suppress_output():
+            self.assertRaises(error.MissingOptionValue, App().test_merge.invoke, ['--some'])
+
+    def test_duplicate_option(self):
+        with suppress_output():
+            self.assertRaises(error.DuplicateOption, App().test_merge.invoke, '-f --flag'.split())
+
+    def test_error_output(self):
+        try:
+            with capture_output() as (out, err):
+                App().test_merge.invoke(['--unknown'])
+        except error.UnknownOption as e:
+            self.assertIn(str(e), out.getvalue())
+            self.assertIn('--unknown', str(e))
+
+    def test_help_on_option_error(self):
+        try:
+            with capture_output() as (out, err):
+                App().test_merge.invoke(['--unknown'])
+        except error.UnknownOption as e:
+            self.assertIn(App.test_merge.full_doc(), out.getvalue())
